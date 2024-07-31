b80775ed9ddd3b3010ea116eccb4d80318ad38a5
PatrycjaJakubow <106872766+PatrycjaJakubow@users.noreply.github.com>
2024-07-31 15:41:08 +0200

    Initial upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+import { DUMMY_USERS } from './dummy-users';
+
+@Component({
+    selector: 'app-root',
+    standalone: false,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+})
+
+
+
+export class AppComponent {
+users = DUMMY_USERS;
+selectedUserId?: string;
+specialTasks = DUMMY_USERS;
+
+get selectedUser() {
+  return this.users.find((user) => user.id === this.selectedUserId);
+}
+
+
+
+onSelectUser(id: string) {
+console.log('selected user ' + id);
+this.selectedUserId = id;
+}
+
+
+}
